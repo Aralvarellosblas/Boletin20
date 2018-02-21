8de02ff16fcf14f2b9ef2b68550c5d810a0b9c27
arturo <arturo@ArturoMSI>
2018-02-21 08:53:01 +0100

    Inicio Ejercicio

---
+++ src/ejercicio20/Ejercicio20.java
@@ -0,0 +1,16 @@
+package ejercicio20;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Ejercicio20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
