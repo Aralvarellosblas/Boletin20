fc4b58b501d1dd25077114a56fd2562bb47b3dc2
arturo <arturo@ArturoMSI>
2018-02-22 14:03:36 +0100

    Añadido menu de seleccion para los metodos

--- src/ejercicio20/Ejercicio20.java
+++ src/ejercicio20/Ejercicio20.java
@@ -1,16 +1,53 @@
 package ejercicio20;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Arturo
  */
-public class Ejercicio20 {
+public class Ejercicio20{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
+    public static void main(String[] args){
+        Libreria li=new Libreria();
+        li.crearLibreria();
+        int x;
+        do{
+            x=Integer.parseInt(JOptionPane.showInputDialog("Selecciona:\n"
+                    +"1-- Añadir libro\n"
+                    +"2-- Consultar Fichero\n"
+                    +"3-- Buscar Libro\n"
+                    +"4-- Borrar libros con 0 unidades\n"
+                    +"5-- Cambiar precio\n"
+                    +"0-- Para salir"));
+            switch(x){
+                case 1:
+                    li.añadirLibro();
+                    li.guardarLibros();
+                    break;
+                case 2:
+                    li.visualizarLibros();
+                    break;
+                case 3:
+                    li.buscarLibro();
+                    break;
+                case 4:
+                    li.borrarLibros();
+                    break;
+                case 5:
+                    li.cambiarPrecio();
+                    li.guardarLibros();
+                    break;
+                case 0:
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Seleccion Incorrecta");
+            }
+        }while(x!=0);
+
     }
 
 }
